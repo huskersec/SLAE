1694485b7083d959b4c09ac2ebe6d8bff6b3d5a1
huskersec <30422513+huskersec@users.noreply.github.com>
2018-09-06 23:40:13 -0400

    Update egghunter_template.c

--- Modules/EggHunter/egghunter_template.c
+++ Modules/EggHunter/egghunter_template.c
@@ -4,20 +4,14 @@
 unsigned char payload[] =
 "";
 
-
 unsigned char egghuntr[] = 
 "";
 
-
 main()
 {
-
 	printf("Shellcode Payload Length:  %d\n", strlen(payload));
-
-
 	int (*ret)() = (int(*)())egghuntr;
 	ret();
-
 }
 
 
